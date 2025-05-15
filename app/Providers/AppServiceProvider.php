0b5bfc50c6038c865b2bdda8e48007a37807842a
Mifaki <agustianto.d19@gmail.com>
2025-05-15 08:26:49 +0700

    feat: add rate limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,6 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Rate Limiter for API call
+        RateLimiter::for('api', fn(Request $request) =>
+            Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
+        );
+
+        // Rate Limiter for authentication
+        RateLimiter::for('auth', fn(Request $request) =>
+            Limit::perMinute(5)->by($request->ip())
+        );
+
+        // Rate limiter for filament daashboard
+        RateLimiter::for('filament', fn(Request $request) =>
+            Limit::perMinute(120)->by($request->user()?->id ?: $request->ip())
+        );
     }
 }
---
+++ storage/framework/views/09791cd782a1cfbb547ac331bf7b0366.php
@@ -0,0 +1,8 @@
+<p
+    <?php echo e($attributes->class(['fi-section-header-description overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</p>
+<?php /**PATH /Users/pais/Documents/Kuliah/Semester 5/capstone/transkripKU/vendor/filament/support/src/../resources/views/components/section/description.blade.php ENDPATH**/ ?>
